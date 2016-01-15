263c9c0115a55f7397d05e3646cf93f776794aa6
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-01-15 18:04:40 -0500

    Initial Commit

---
+++ Scripts/core/game.ts
@@ -0,0 +1,15 @@
+/// <reference path="_reference.ts" />
+
+import Person = objects.Person;
+import Student = objects.Student;
+
+
+console.log("Game Started...");
+
+
+var person: Person = new Person("Tom");
+person.speaks();
+
+var student: Student = new Student("Mike", 123456);
+student.studies();
+student.speaks();
